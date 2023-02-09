463e014c44b60c53d95694c9f170111ab9fb6fe3
Nnamdi Anyanwu <blueking189@gmail.com>
2023-02-09 23:53:42 +0100

    Update return syntax

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -6,10 +6,12 @@ CREATE FUNCTION SafeDiv (a INT, b INT)
         RETURNS FLOAT
         DETERMINISTIC
                 BEGIN
+                      DECLARE res FLOAT
                       IF b=0 THEN
                          RETURN 0;
                       ELSE
-                         RETURN ((a * 1.0) / b);
+                         SET res = ((a * 1.0) / b);
+                         RETURN res;
                     END IF;
                   END; $$
 DELIMITER ;
