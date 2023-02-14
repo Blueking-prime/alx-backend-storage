b70301be3816b7b3af4a5893b4c1d5e25781803b
Nnamdi Anyanwu <blueking189@gmail.com>
2023-02-14 11:52:14 +0100

    More syntax and typo fixes

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -4,8 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     '''returns the list of school having a specific topic'''
-    documents = mongo_collection.find()
-    if documents:
-        return [doc for doc in documents if topic in doc.topics]
-    else:
-        return []
+    documents = mongo_collection.find({'topics': topic})
